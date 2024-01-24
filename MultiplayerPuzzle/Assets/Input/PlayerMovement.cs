a203f50e3215bf11109c803d0ca0ab8056b2a683
Robyn <92083124+multidimensionalsock@users.noreply.github.com>
2024-01-24 10:30:31 +0000

    MVOEMEMNT VIA SERVER???????!?!?!?!?!?!?!?!?!?!?

--- MultiplayerPuzzle/Assets/Input/PlayerMovement.cs
+++ MultiplayerPuzzle/Assets/Input/PlayerMovement.cs
@@ -13,41 +13,58 @@ public class PlayerMovement : NetworkBehaviour
     [SerializeField] float m_moveForce;
     [SerializeField] float maxSpeed;
     Rigidbody2D m_rigidbody;
+    int rasndom;
 
     // Start is called before the first frame update
     void Start()
     {
+        PingServerRpc();
         m_input = GetComponent<PlayerInput>();
         m_rigidbody = GetComponent<Rigidbody2D>();
         m_input.currentActionMap.FindAction("Move").performed += MoveStart;
         m_input.currentActionMap.FindAction("Move").canceled += MoveEnd;
+        rasndom = Random.Range(0, 100);
     }
 
     void MoveStart(InputAction.CallbackContext context)
     {
-        if (!IsOwner) { return;  }
+        if (!IsLocalPlayer) { return;  }
         m_moveDirection = context.ReadValue<Vector2>();
         StartCoroutine(Move());
     }
 
     void MoveEnd(InputAction.CallbackContext context)
     {
-        if (!IsOwner) { return; }
+        if (!IsLocalPlayer) { return; }
         m_moveDirection = Vector2.zero;
     }
 
     IEnumerator Move()
     {
-        
         while(m_moveDirection != Vector2.zero)
         {
             Debug.Log(m_moveDirection);
-            m_rigidbody.AddForce( m_moveForce * Time.fixedDeltaTime * m_moveDirection);
+            m_rigidbody.AddForce(m_moveForce * Time.fixedDeltaTime * m_moveDirection);
             m_rigidbody.velocity = Vector3.ClampMagnitude(m_rigidbody.velocity, maxSpeed);
+
+            MoveCharacterServerRpc(m_rigidbody.velocity);
             //set max force to move force and it keeps about the same
             //set to zero vector at end of this if you want to just stop. or could set velocity lower?like 0.1?
             yield return new WaitForFixedUpdate();
         }
         m_rigidbody.velocity = Vector2.zero;
+        MoveCharacterServerRpc(m_rigidbody.velocity);
+    }
+
+    [ServerRpc]
+    void MoveCharacterServerRpc(Vector2 velocity)
+    {;
+        m_rigidbody.velocity = velocity;
+    }
+
+    [ServerRpc]
+    public void PingServerRpc()
+    {
+        Debug.Log("Working on the server" + rasndom);
     }
 }
